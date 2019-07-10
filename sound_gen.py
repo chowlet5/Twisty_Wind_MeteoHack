7c3b2c095a851f2aa5c08ff70067568216aaedea
chowlet5 <howlett2306@rogers.com>
2019-07-10 17:57:48 -0400

    Updated Functions

--- sound_gen.py
+++ sound_gen.py
@@ -3,22 +3,40 @@ import requests
 import pandas as pd
 import numpy as np
 import datetime
+import time
+
+
+from pyo import *
+import time
+
+import math
+
+#Location Variables
+
+
+province = 'ON'
+station = 6016975
 
 
 
 def grab_data(prov,station):
-
+    
     url = fr'https://dd.weather.gc.ca/climate/ahccd/geojson/historical/monthly/{prov}'
-    file_start = f'AHCCD_hist_monthly_{prov}_{station}'
-    print(url)
+    
+    file_start = []
+    for stat_adjust in np.arange(-5,6):
+        file_start.append( f'AHCCD_hist_monthly_{prov}_{station+stat_adjust}')
+    
 
     def listFD(url,file_start=''):
         page = requests.get(url).text
 
         soup = BeautifulSoup(page, 'html.parser')
         return [url + '/' + node.get('href') for node in soup.find_all('a') if node.get('href').startswith(file_start)]
-
-    file_list = listFD(url,file_start)
+    
+    file_list = []
+    for file_s in file_start:
+        file_list.extend(listFD(url,file_s))
 
 
     first = True
@@ -33,7 +51,7 @@ def grab_data(prov,station):
             for row in raw_data.features:
                 year = row['properties']['year']
                 month = row['properties']['period']
-                data.append([datetime.datetime.strptime(f'{year} {month}' , '%Y %b'),row['properties']['value']])
+                data.append([datetime.datetime.strptime(f'{year} {month}' , '%Y %b'),float(row['properties']['value'])])
 
             data_type = raw_data.features[0]['properties']['measure_type']
             df = pd.DataFrame(data,columns =['year',data_type])
@@ -46,110 +64,180 @@ def grab_data(prov,station):
             for row in raw_data.features:
                 year = row['properties']['year']
                 month = row['properties']['period']
-                data.append([datetime.datetime.strptime(f'{year} {month}' , '%Y %b'),row['properties']['value']])
+                data.append([datetime.datetime.strptime(f'{year} {month}' , '%Y %b'),float(row['properties']['value'])])
 
             data_type = raw_data.features[0]['properties']['measure_type']
             df_temp = pd.DataFrame(data,columns =['year',data_type])
             df_temp = df_temp.set_index('year')
             df_temp = df_temp.sort_index()
+            df_temo = df_temp.replace(-9999.9,0)
             df = pd.concat([df,df_temp],axis = 1, join_axes=[df.index])
     
     df = df.replace(-9999.9,np.nan)
 
-
+    df = df[df.index>=datetime.datetime(1970,4,1)]
 
     return df
 
 
-def parameter_update(climate_data):
-    snds3 = ['snds/alum1.wav', 'snds/alum2.wav',
-        'snds/alum3.wav', 'snds/alum4.wav']
+def parameter_update_rain(climate_data):
+    snds3 = ['snds/alum2.wav']
     tabs = SndTable(snds3)
 
 
-    beat = Metro(time=float(value), poly=3).play()
+    beat = Metro(float(climate_data), poly=3).play()
     wav = SquareTable()
     t = CosTable([(0,0), (100,1), (2000,.3), (8191,0)])
     amp = TrigEnv(beat, table=tabs, dur=.25, mul=.3)
-    pitch = TrigXnoiseMidi(beat,dist = 4,mrange = (48,48))
+    pitch = TrigXnoiseMidi(beat,dist = 4,mrange = (28,48))
 
     return [wav,pitch,amp]
 
+def parameter_update_snow(climate_data):
+    
+    ind = LinTable([(0,3), (20,40), (300,10), (1000,5), (8191,3)])
+    m = Metro(0.9+climate_data).play()
+    tr = TrigEnv(m, table=ind, dur=0.5)
 
 
-from pyo import *
-import time
-import numpy as np
+    return tr
+
+def parameter_update_wind(climate_data):
+    a = Sine(.25, 0, .1, .1)
+    
+    noise = PinkNoise(a/100+climate_data).mix(2)
+
+    return noise
+
+recorder = True
+
+
+# Main Section
+
+
+df = grab_data(province,station)
 
 num = 0
-
-
-s = Server().boot().start()
-snds3 = ['snds/alum1.wav', 'snds/alum2.wav',
-        'snds/alum3.wav', 'snds/alum4.wav']
-
-tabs = SndTable(snds3)
-'''
-beat = Metro(time=0.5, poly=3).play()
-wav = SquareTable()
-t = CosTable([(0,0), (100,1), (2000,.3), (8191,0)])
-amp = TrigEnv(beat, table=tabs, dur=.25, mul=.3)
-pitch = TrigXnoiseMidi(beat,dist = 4,mrange = (48,48))
+stop = len(df.index)-1
+print(stop)
+s = Server()
+s.boot().start()
 
 
 
-rain = Osc(table=wav,freq= pitch ,mul = amp).out()
-'''
 
-tabs = SndTable(snds3)
+if 'rain' in df.columns.values:
 
-beat_snow = Metro(time=0.5, poly=3).play()
-wav_snow = SquareTable()
-t_snow = CosTable([(0,0), (100,1), (2000,.3), (8191,0)])
-amp_snow = TrigEnv(beat_snow, table=tabs, dur=.25, mul=.1)
-pitch_snow = TrigXnoiseMidi(beat_snow,dist = 4,mrange = (100,100))
+    snds3 = ['snds/alum2.wav']
+
+    tabs = SndTable(snds3)
+
+    beat = Metro(time=0.5, poly=5).play()
+    wav = SquareTable()
+    t = CosTable([(0,0), (100,1), (2000,.3), (8191,0)])
+    amp = TrigEnv(beat, table=tabs, dur=.25, mul=.1)
+    pitch = TrigXnoiseMidi(beat,dist = 2,mrange = (24,48))
 
 
 
-snow = Osc(table=wav_snow,freq= pitch_snow ,mul = amp_snow).out()
+    rain = Osc(table=wav,freq= pitch ,mul = amp*1.0).out(dur=stop*1.5)
+
+
+
+if 'snow' in df.columns.values:
+    snow_diff = float(df['snow'].max()) - float(df['snow'].min())
+    snow_data = 1.333*(pd.to_numeric(df['snow']) - float(df['snow'].mean()))/float(snow_diff)
+
+    ind = LinTable([(0,3), (20,40), (300,10), (1000,5), (8191,3)])
+    m = Metro(.9).play()
+    tr = TrigEnv(m, table=ind, dur=0.5)
+    snow = FM(carrier=[251,250], ratio=[.2498,.2503], index=tr, mul=.02).out(dur=stop*1.5)
+
+
+if 'wind_speed' in df.columns.values:
+
+    a = Sine(.25, 0, .1, .1)
+    noise = PinkNoise(a/100+0.01).mix(2)
+    tone = Tone(noise,1000).out(dur=stop*1.5)
+
+
+
+if 'temp_mean' in df.columns.values:
+    temp_diff = float(df['temp_mean'].max()) - float(df['temp_mean'].min())
+    temp_data = 0.2*(pd.to_numeric(df['temp_mean']) - float(df['temp_mean'].mean()))/float(temp_diff)
+
+
+    lfo = Sine([2,5], 0, .1, .1)
+    sine_loop = Sine(freq=[400], mul=.1).out(dur=stop*1.5)
+
 
 
 
 def pcp_temp():
     
     global num
-    
+
     #Update Rain Parameters
+    
+    x_rain = float(df['rain'].values[num])
 
-    x = df['Rain'].values[num]
-
-    if x != np.nan:
-        value = x
+    if x_rain <= 0.0:
+        value_rain = 0.0
     else:
-        value = 1
+        value_rain = 1-(float(x_rain)/(1.1*float(df['rain'].max())))
 
-    new_para = parameter_update(1-value)
+   
+
+    if 0< value_rain < 0.1:
+        value_rain = 0.1
+    
+
+    new_para = parameter_update_rain(value_rain)
 
     rain.table = new_para[0]
     rain.freq = new_para[1]
     rain.mul = new_para[2]
     
     
-    x_snow = df_snow['Snow'].values[num]
+    x_snow = float(snow_data[num])
 
-    if x_snow != np.nan:
-        value_snow = x_snow
+    if x_snow <= 0.0:
+        value_snow = -0.9
     else:
-        value_snow = 1
-    print(1-value_snow)
+        value_snow = x_snow
 
-    new_para = parameter_update(1-value_snow)
-    snow.table = new_para[0]
-    snow.freq = new_para[1]
-    snow.mul = new_para[2]
+
+    
+    snow.index=parameter_update_snow(value_snow)
+    
+
+    
+    x_wind = float(df['wind_speed'].values[num])
+    print(x_wind)
+    if x_wind <= 0.0 or math.isnan(df['wind_speed'].values[num]):
+        value_wind = 0
+    else:
+        value_wind = (float(x_wind)/(1.1*float(df['wind_speed'].max())))
+
+    print(value_wind)
     
     
+    tone.setInput(parameter_update_wind(value_wind/30))
+    
+    
+    x_temp = temp_data[num]
+    
+    print(x_temp)
+    if not math.isnan(x_temp):
+        sine_loop.mul = float(0.1+x_temp)
+        sine_loop.freq = [200+x_temp*2000]
+    print(num)
     num+=1
 
 
-p =Pattern(pcp_temp,2).play()
+p =Pattern(pcp_temp,1.5)
+x= p.play(stop*1.5)
+
+
+
+
